e1c1d217f98d3bdfff94309c2c3fc382e6b87fca
yoda-jedi <117478434+yoda-jedi@users.noreply.github.com>
2023-03-05 23:18:39 +0530

    Update math.js

--- math.js
+++ math.js
@@ -1,1 +1,12 @@
 console.log(Math)
+
+Math.round(7.7);  // .floor, .ceil, .random
+
+function floor(x){
+  a,b = x.split(".");
+  if(b > 5) return Math.round(x) -1
+  else return Math.round(x);
+}
+
+// fusiom
+Math.round(Math.random()*5);
